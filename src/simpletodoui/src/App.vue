8c40f5ff43ec1122eed4f4ed2ea278a2d07b47a3
Michał Mrzygłód <mmrzyglod1@gmail.com>
2024-05-07 21:24:33 +0200

    Added top panel

--- src/simpletodoui/src/App.vue
+++ src/simpletodoui/src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <TopPanel />
   <router-view />
 </template>
 
+<script  lang="ts">
+import { defineComponent } from "vue";
+import TopPanel from "@/components/TopPanel.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TopPanel,
+  },
+});
+</script>
+
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,8 +30,6 @@
 }
 
 nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/simpletodoui/src/components/TopPanel.vue
@@ -0,0 +1,38 @@
+﻿<template>
+  <div class="top-panel">
+    <div class="logo">
+      <strong><router-link to="/">SimpleTodo</router-link></strong>
+    </div>
+    <div>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/about">Login</router-link>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "TopPanel"
+});
+</script>
+
+<style scoped lang="scss">
+.top-panel {
+  display: flex;
+  justify-content: space-between;
+  background: lightgrey;
+  padding: 0.5rem;
+  
+  .logo {
+    a {
+      text-decoration: none;
+      color: black;
+    }
+  }
+}
+</style>
